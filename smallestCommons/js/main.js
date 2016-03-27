9bb8edf6e4f581bcb6ab6ca7c767405e53e0e71b
J. Cohen <hostelmarketing@gmail.com>
2016-03-27 11:13:04 -0700

    Almost done with improved algorithm

--- smallestCommons/js/main.js
+++ smallestCommons/js/main.js
@@ -1,55 +1,99 @@
-// Return an array with the specified range
-var range = function (start, end) {
-    var i, arr = [];
-    for (i = start; i <= end; i++) {
-        arr.push(i);
+// Take a 2-element array and return an ascending range
+let ascendingRange = function (arr) {
+    let from, to,
+        range = [];
+
+    // Make sure that it's lowest to highest
+    arr[0] < arr[1] ? [from, to] = arr : [from, to] = arr.reverse();
+    for (let i = from; i <= to; i++) {
+        range.push(i);
     }
-    return arr;
+    return range;
+};
+
+// From the pseudocode here: https://en.wikipedia.org/wiki/Primality_test#Pseudocode
+let isPrime = function (num) {
+    let i = 5;
+
+    if (num <= 1) {
+        return false;
+    } else if (num <= 3) {
+        return true;
+    } else if ((num % 2 === 0) || (num % 3 === 0)) {
+        return false;
+    }
+
+    while (i * i <= num) {
+        if ((num % i === 0) || (num % (i + 2) === 0)) {
+            return false;
+        }
+        i += 6;
+    }
+
+    return true;
+};
+
+// Return an array of prime factors for num
+let getPrimeFactors = function (num) {
+    let factors = [];
+    if (num < 2) {
+        return [];
+    }
+    for (let i = 2; i <= num; i++) {
+        while (num % i === 0) {
+            factors.push(i);
+            num /= i;
+        }
+    }
+    return factors;
 };
 
 // Multiply all the elements of a given array
-var multiplyArr = function (arr) {
-    var product = arr.reduce(function (a, b) {
+let multiplyArr = function (arr) {
+    let product = arr.reduce(function (a, b) {
         return a * b;
     });
     return product;
 };
 
-// Test a target number. Return false if not divisible by every element of the array
-var testCurrentTarget = function (target, arr) {
-    console.log('running testCurrentTarget with: ', target + ' & ' + arr);
-    for (var i = arr[arr.length-1]; i >= arr[0]; i--) {
-        console.log(`i is ${i}. The target is ${target}. Checking if ${target} % ${i} is true or false`);
-        if (target % i !== 0) {
-            console.log('about to return false');
-            return false;
+// Take an array of arrays and return a flattened version
+let flattenArray = function (arr) {
+    [].concat.apply([], arr);
+}
+
+// Usage `arr.filter(onlyUnique)`
+var onlyUnique = function (value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+let smallestCommons = function (arr) {
+    let multiples = ascendingRange(arr);
+
+    // Find prime factors of all numbers in range
+    let primeFactors = [];
+    multiples.forEach(function (n) {
+        let pf = getPrimeFactors(n);
+        if (pf.length > 0) {
+            primeFactors.push(pf);
         }
-    }
-    console.log('about to return true');
-    return true;
+    });
+
+    // Filter to a unique list
+    let flattenedPrimeFactors = [].concat.apply([], primeFactors);
+    let uniqPrimeFactors = flattenedPrimeFactors.filter(onlyUnique);
+
+    // Find the largest amounts of each prime factor
+
+    // TODO: replace this with dynamic array
+    let finalPrimeFactors = [2, 3, 5, 7, 11, 13]
+    // Multiply the prime factors for the answer
+    return multiplyArr(finalPrimeFactors);
 };
 
-var smallestCommons = function (a) {
-    // create an array with the appropriate range
-    arrMax = Math.max(a[0], a[1]);
-    arrMin = Math.min(a[0], a[1]);
-    var r = range(arrMin, arrMax);
-    console.log('r', r);
-    var total = multiplyArr(r);
-    //var total = arrMax * arrMin;
-    var i, currentTarget;
-
-    // Outer loop goes backwards
-    for (i = arrMax; i >= arrMin; i--) {
-        currentTarget = total / i;
-        console.log(`About to check ${total}/${i}, which is ${currentTarget}`);
-        if (testCurrentTarget(currentTarget, r)) {
-            return currentTarget;
-        }
-        console.log('-------');
-    }
+let main = function () {
+    let answer = smallestCommons([1, 13]);
+    let el = document.getElementById('outputArea');
+    el.innerText = answer;
 };
-var answer = smallestCommons([1, 6]);
-var el = document.getElementById('outputArea');
-el.innerText = answer;
-//smallestCommons([1, 5]);
+
+main();
