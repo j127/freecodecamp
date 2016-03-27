1bb18bcf4352cf7409324ea928666db787b1ad02
J. Cohen <hostelmarketing@gmail.com>
2016-03-27 11:31:38 -0700

    Move to arrow functions. Add comments

--- smallestCommons/js/main.js
+++ smallestCommons/js/main.js
@@ -1,5 +1,5 @@
 // Take a 2-element array and return an ascending range
-let ascendingRange = function (arr) {
+let ascendingRange = (arr) => {
     let from, to,
         range = [];
 
@@ -12,7 +12,7 @@ let ascendingRange = function (arr) {
 };
 
 // From the pseudocode here: https://en.wikipedia.org/wiki/Primality_test#Pseudocode
-let isPrime = function (num) {
+let isPrime = (num) => {
     let i = 5;
 
     if (num <= 1) {
@@ -34,7 +34,7 @@ let isPrime = function (num) {
 };
 
 // Return an array of prime factors for num
-let getPrimeFactors = function (num) {
+let getPrimeFactors = (num) => {
     let factors = [];
     if (num < 2) {
         return [];
@@ -49,29 +49,24 @@ let getPrimeFactors = function (num) {
 };
 
 // Multiply all the elements of a given array
-let multiplyArr = function (arr) {
-    let product = arr.reduce(function (a, b) {
+let multiplyArr = (arr) => {
+    let product = arr.reduce((a, b) => {
         return a * b;
     });
     return product;
 };
 
-// Take an array of arrays and return a flattened version
-let flattenArray = function (arr) {
-    [].concat.apply([], arr);
-}
-
 // Usage `arr.filter(onlyUnique)`
-var onlyUnique = function (value, index, self) {
+var onlyUnique = (value, index, self) => {
     return self.indexOf(value) === index;
 }
 
-let smallestCommons = function (arr) {
+let smallestCommons = (arr) => {
     let multiples = ascendingRange(arr);
 
     // Find prime factors of all numbers in range
     let primeFactors = [];
-    multiples.forEach(function (n) {
+    multiples.forEach((n) => {
         let pf = getPrimeFactors(n);
         if (pf.length > 0) {
             primeFactors.push(pf);
@@ -83,14 +78,27 @@ let smallestCommons = function (arr) {
     let uniqPrimeFactors = flattenedPrimeFactors.filter(onlyUnique);
 
     // Find the largest amounts of each prime factor
+    uniqPrimeFactors.forEach((n) => {
+        console.log('n', n);
+        // Loop over primeFactors.
+        // Count the number of times `n` occurs in that group of prime factors
+        // If it's larger than the highest number of seen `n`s, then replace
+        // {
+        //      '2': 3,
+        //      '3': 2,
+        //      '5': 5
+        // }
+    });
 
     // TODO: replace this with dynamic array
-    let finalPrimeFactors = [2, 3, 5, 7, 11, 13]
+    let finalPrimeFactors = [2, 2, 2, 3, 3, 5, 7, 11, 13]
     // Multiply the prime factors for the answer
     return multiplyArr(finalPrimeFactors);
 };
 
-let main = function () {
+
+// Do stuff to the DOM
+let main = () => {
     let answer = smallestCommons([1, 13]);
     let el = document.getElementById('outputArea');
     el.innerText = answer;
